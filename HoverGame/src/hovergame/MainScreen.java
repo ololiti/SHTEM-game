cddc2378807c63f9aea76b6c8bde5a7af319d326
ololiti <aditi.pluto@gmail.com>
2019-07-01 11:57:12 -0700

    added an instruction panel

--- HoverGame/src/hovergame/MainScreen.java
+++ HoverGame/src/hovergame/MainScreen.java
@@ -17,11 +17,26 @@ public class MainScreen extends JFrame{
     public static final int WIDTH = 500;
     public static final int HEIGHT = 500;
     public static final Color BACKGROUND = new Color(232,241,242);
+    public static final Font TEXT_FONT= new Font("Courier", Font.BOLD, 12);
+    
     public MainScreen(){
-        super("Game of Life");
+        super("Hover Game");
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(3,1));
+        setLayout(new GridLayout(4,1));
         setBackground(BACKGROUND);
+        
+        JPanel instructions = new JPanel();
+        instructions.setLayout(new FlowLayout());
+        instructions.setBackground(BACKGROUND);
+        JTextArea instructionText = new JTextArea();
+        instructionText.setFont(TEXT_FONT);
+        instructionText.setText("Hover over a panel to see the point (-1). "
+                + "\nClick to move the point to the center (-10)."
+                + "\nDon't let the point leave the bounds (-100).");
+        instructionText.setEditable(false);
+        instructions.add(instructionText);
+        add(instructions);
+        
     }
 }
