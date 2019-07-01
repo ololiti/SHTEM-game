bd2b2ec2931deea94fe72e9313c33b86ebf8a145
ololiti <aditi.pluto@gmail.com>
2019-07-01 12:33:23 -0700

    added ovals

--- HoverGame/src/hovergame/MainScreen.java
+++ HoverGame/src/hovergame/MainScreen.java
@@ -16,19 +16,21 @@ import java.awt.event.*;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.Random;
-public class MainScreen extends JFrame implements MouseListener{
+public class MainScreen extends JFrame {
     private static int size = 50;
     public static final int WIDTH = 500;
     public static final int HEIGHT = 500;
     public static final Color BACKGROUND = new Color(232,241,242);
     public static final Font TEXT_FONT= new Font("Courier", Font.BOLD, 12);
-    public static final int DIAMETER = 3;
+    public static final int DIAMETER = 10;
     public static final int TIME = 250;
+    public static final int Y = 25;
+    public static final int X = 250;
     
     private int points = 0;
-    private int x1 = 0;
-    private int x2 = 0;
-    private int x3 = 0;
+    private int x1 = X;
+    private int x2 = X;
+    private int x3 = X;
     
     public MainScreen(){
         super("Hover Game");
@@ -51,9 +53,22 @@ public class MainScreen extends JFrame implements MouseListener{
         
         JPanel game1 = new JPanel(){
             public void paintComponent(Graphics g){
-                g.drawOval(x1, 0, DIAMETER, DIAMETER);
+                g.drawOval(x1, Y, DIAMETER, DIAMETER);
             }
         };
+        add(game1);
+        JPanel game2 = new JPanel(){
+            public void paintComponent(Graphics g){
+                g.drawOval(x2, Y, DIAMETER, DIAMETER);
+            }
+        };
+        add(game2);
+        JPanel game3 = new JPanel(){
+            public void paintComponent(Graphics g){
+                g.drawOval(x3, Y, DIAMETER, DIAMETER);
+            }
+        };
+        add(game3);
         
         /*TimerTask task = new TimerTask()
         {
