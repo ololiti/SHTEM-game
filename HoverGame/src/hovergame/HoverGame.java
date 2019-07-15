578e89dfd382e478bc1252012d039ad7c17ae969
karan4762 <52425017+karan4762@users.noreply.github.com>
2019-07-15 14:01:34 -0700

    Update HoverGame.java

--- HoverGame/src/hovergame/HoverGame.java
+++ HoverGame/src/hovergame/HoverGame.java
@@ -1,5 +1,6 @@
 /*
  * Aditi Talati - 1 July 2019
+ * Karan Bhasin- 15 July 2019
  * create a game where people have to keep a point in a range, but they lose 
  * points every time they look at the point
  */
